1d4c011822513931690cc9f78bcc46ff859a7133
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2023-02-13 16:08:54 +0100

    Add files via upload

--- include/aux_funs.h
+++ include/aux_funs.h
@@ -3,11 +3,11 @@
 /* builds the workspace out of the dataset */
 int set_workspace(struct system_t * system, char * name);
 
-/* prints the results*/
+/* prints the results obtained */
 void print_results(struct system_t * system, FILE * fp);
 
 /* produces a file with the results */
 int produce_file(struct system_t * system);
 
-/* produces graphics using gnuplot*/
-void produce_graphics_gnuplot(struct system_t * system);
+/* produces graphics out of the results using gnuplot*/
+int produce_graphics_gnuplot(struct system_t * system);
--- include/data_struct.h
+++ include/data_struct.h
@@ -1,21 +1,21 @@
 /* data_struct.h*/
 #define ERROR_FILE 1
-#define ERROR_MEASURES 2
-#define ERROR_DIR 3
+#define ERROR_DIR 2
+#define ERROR_FORMAT 2
 
-/* structure of system*/
+/* structure of the system*/
 struct system_t
 {
 	unsigned int * cpus; /* cpu amount */
-	double * runtime; /* average time per processor */
-	double * speedup; /* speed-up achieved per processor */
-	double * efficiency; /* efficiency per processor */
+	double * runtime; /* average runtime per processor */
+	double * speedup; /* speed up achieved per processor */
+	double * efficiency; /* efficiency achieved per processor */
 
 	unsigned int ncpus; /* number of processors */
-	unsigned int nruns; /* number of runs */
 	unsigned int hasheader; /* 0: dataset file lacks header, 1: dataset file has header */
 };
 
+// data_struct.c functions
 void initialize_system(struct system_t * system);
 void free_system(struct system_t * system);
 
